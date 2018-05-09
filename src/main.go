450847ea5206c2fb71ce8dfb136e5782280a3fd1
croacker <croacker@mail.ru>
2018-05-09 23:21:44 +0800

    add bindata

--- src/main.go
+++ src/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"./httpserver"
 )
 
 func main() {
+	resource, err := Asset("static/templates/index.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(resource)
+
 	httpserver.Start()
 }
